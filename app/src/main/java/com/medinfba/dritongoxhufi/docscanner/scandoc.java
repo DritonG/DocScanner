a3f936c656c136bd69f6c235aa2e5c9b946767c9
DritonG <driton-goxhufi@web.de>
2016-05-25 17:50:54 +0200

    Initial commit

---
+++ app/src/main/java/com/medinfba/dritongoxhufi/docscanner/scandoc.java
@@ -0,0 +1,13 @@
+package com.medinfba.dritongoxhufi.docscanner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class scandoc extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scandoc);
+    }
+}
